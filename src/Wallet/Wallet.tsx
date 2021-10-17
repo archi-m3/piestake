00a13c28147b2e178963bbe1b5de3887fd40e93f
Archi <archi.m3@protonmail.com>
2021-10-16 17:43:28 -0700

    Check rewards

--- src/Wallet/Wallet.tsx
+++ src/Wallet/Wallet.tsx
@@ -1,10 +1,34 @@
-import React, {useState} from "react";
-import {Button, Row, Col, Card} from "antd";
+import React, {useCallback, useEffect, useState} from "react";
+import {Button, Divider, Row, Col, Card, Spin, Timeline} from "antd";
 import {baseAddrToStakeAddr} from "../utils";
 import './Wallet.less';
 
+import type {Reward} from "./Wallet.types";
+
+const pieverseUrlRoot = 'https://api.pieverse.workers.dev';
+
 function Wallet() {
-  const [stakeAddress, setStakeAddress] = useState<string>();
+  const [stakeAddr, setStakeAddress] = useState<string>();
+  const [isLoadingRewards, setIsLoadingRewards] = useState(false);
+  const [rewards, setRewards] = useState<Array<Reward>>();
+
+  const maybeLoadRewards = useCallback(
+    async () => {
+      if (!stakeAddr) return;
+      try {
+        setIsLoadingRewards(true);
+        const url = `${pieverseUrlRoot}/rewards?addr=${stakeAddr}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setRewards(data);
+      } finally {
+        setIsLoadingRewards(false);
+      }
+    },
+    [stakeAddr]
+  );
+
+  useEffect(() => {maybeLoadRewards();}, [stakeAddr]);
 
   const onConnect = async () => {
     const isEnabled = await window.cardano.isEnabled();
@@ -22,25 +46,50 @@ function Wallet() {
     color: '@black'
   };
 
-  const title = stakeAddress ? "Connected to wallet" : "Connect to Nami Wallet";
+  const toAda = (lovelace: string) => {
+    lovelace = lovelace.padStart(7, '0');
+    return `${lovelace.substring(0, lovelace.length - 6)}.${lovelace.substring(lovelace.length - 6)}`;
+  };
 
-  const connectButton = !stakeAddress && (
-    <Button type="primary" shape="round" size="large" onClick={onConnect}>
-      Connect
-    </Button>
-  );
+  const cardContent = (): React.ReactNode => {
+    if (isLoadingRewards) {
+      return <Spin/>;
+    } else if (stakeAddr) {
+      const cardanoScanLink = `https://cardanoscan.io/stakekey/${stakeAddr}`;
+      return (
+        <>
+          Stake Address: <a href={cardanoScanLink} target="_blank">{stakeAddr}</a>
+          <Divider/>
+          <Timeline mode='left' reverse={true}>
+            {rewards?.map(r => (
+              <Timeline.Item key={r.epoch} label={`${r.epoch}`}>
+                ₳ {toAda(r.amount)}
+                <br/>
+                {r.pool_id}
+              </Timeline.Item>
+            ))}
+          </Timeline>
+        </>
+      )
+    } else if (!stakeAddr) {
+      return (
+        <Button type="primary" shape="round" size="large" onClick={onConnect}>
+          Connect to Nami wallet
+        </Button>
+      );
+    }
+  };
 
   return (
     <section id="wallet" className="wallet">
       <Row>
         <Col lg={{span: 12, offset: 6}} span={24}>
           <Card
-            title={title}
+            title="Check Rewards"
             className="walletCard"
             headStyle={headStyle}
             bordered={false}>
-            {connectButton}
-            {stakeAddress && 'Stake Address: ' + stakeAddress}
+            {cardContent()}
           </Card>
       </Col>
       </Row>
---
+++ src/Wallet/Wallet.types.ts
@@ -0,0 +1,5 @@
+export type Reward = {
+  amount: string;
+  epoch: number;
+  pool_id: string;
+};
